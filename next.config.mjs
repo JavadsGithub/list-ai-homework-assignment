22d5abdd937fc0c393bbe9fb4f842ed025d97faf
Javad Mohammadi <javadsemail@gmail.com>
2024-10-09 14:30:56 +0330

    feat: add required `environment variables`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    ACCESS_TOKEN: "3o1X#052Co",
+    REFRESH_TOKEN: "fg0(NhU6P&|1",
+    USER_DATA: "(8.6|£wFy:59",
+    BASE_URL: "https://dummyjson.com/",
+    ENCRYPTION_SALT: "£A78sa3.:71q",
+  },
+};
 
 export default nextConfig;
